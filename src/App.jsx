7ecdb09459d7d545266b6a66d324f298bc03e6a1
Hamzzu <wjdgudwn36@gmail.com>
2026-05-06 11:36:10 +0000

    영화 목록 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Hookstructure from './Hookstructure'
 import ItemList from './ItemList'
 import TimerSwitch from './TimerSwitch'
+import MovieList from './MovieList'
 
 const App = () => {
   return (
     <div>
-     <TimerSwitch />
+     <MovieList />
     </div> 
   )
 }
